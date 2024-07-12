fea84b288f1a3547f8b3cc9d5ab8ec184968f528
dugite-git <tubasa283one@gmail.com>
2024-07-12 23:13:40 +0900

    あああ

---
+++ bot/cogs/omikuji.py
@@ -0,0 +1,17 @@
+import random
+from discord import app_commands
+from discord.ext import commands
+import discord
+import os
+
+guild_ids = os.getenv('GUILDS')
+class omikujicommand(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(name="omikuji")
+    @discord.app_commands.guilds(guild_ids)
+    async def omikuji_command(self, interaction: discord.Interaction):
+        """おみくじを引く."""
+        choice = random.choice(['大吉', '吉', '小吉', '凶', '大凶'])
+        await interaction.response.send_message(f"あなたの今日の運勢は **{choice}** です!")
--- bot/main.py
+++ bot/main.py
@@ -1,29 +1,23 @@
 import discord
-import random
-import json
-import asyncio
 from discord.ext import commands
 import os
 
+from cogs.omikuji import omikujicommand
+
 BOT_COMMANDS = []
-DISCORD_BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
-
-class MyClient(commands.Bot):
-    def __init__(self, command_prefix="/", *args, **kwargs):
-        super().__init__(command_prefix="/", *args, **kwargs)
-
-    async def on_ready(self):
-        print(f"{self.user}として接続しました。")
-
-    async def on_message(self, message: discord.Message):
-        if message.author == self.user:
-            return
-
-        print(f"{message.author}よりメッセージを受信しました: {message.content}")
-        await super().on_message(message)
+DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+GUILDS = os.getenv("GUILDS")
 
 intents = discord.Intents.all()
-client = MyClient(intents=intents)
-for command in BOT_COMMANDS:
-    client.add_command(command)
-client.run(DISCORD_BOT_TOKEN)
+bot = commands.Bot(command_prefix="/", intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user}として接続しました。")
+    for command in BOT_COMMANDS:
+        await bot.add_cog(command(bot))
+    await bot.tree.sync(guild=discord.Object(id=GUILDS))
+
+
+bot.run(DISCORD_BOT_TOKEN)
