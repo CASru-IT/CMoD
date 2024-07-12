b9e4fac1537858cc7b7b6e891d60b229311a3306
dugite-git <tubasa283one@gmail.com>
2024-07-12 21:16:28 +0900

    Update main.py

---
+++ bot/main.py
@@ -0,0 +1,33 @@
+import discord
+import random
+import json
+import asyncio
+from discord.ext import commands
+import os
+
+BOT_COMMANDS = []
+DISCORD_BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+
+class MyClient(commands.Bot):
+    def __init__(self, command_prefix="/", *args, **kwargs):
+        super().__init__(command_prefix="/", *args, **kwargs)
+
+    async def on_ready(self):
+        print(f"{self.user}として接続しました。")
+
+    async def on_message(self, message: discord.Message):
+        if message.author == self.user:
+            return
+
+        print(f"{message.author}よりメッセージを受信しました: {message.content}")
+        await super().on_message(message)
+
+
+with open(".discord_token") as f:
+    token = f.read().strip()
+
+intents = discord.Intents.all()
+client = MyClient(intents=intents)
+for command in BOT_COMMANDS:
+    client.add_command(command)
+client.run(DISCORD_BOT_TOKEN)
